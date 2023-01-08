55eedc8c1685aced797c0479e86996d8abcd711d
Vivek Sutar <viveksutar26@gmail.com>
2023-01-08 13:07:30 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'two-way-data-binding';
+
+  Country = "INDIA";
+
+}
